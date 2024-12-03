740432d2aeef04d6dba57558ef6c3ffd323b5a5d
Wout Neirynck <wout@monkey-projects.be>
2024-12-03 14:28:24 +0100

    Dependency updates + add github plugin

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,3 +1,7 @@
-(require '[monkey.ci.plugin.clj :as p])
+(ns build
+  (:require [monkey.ci.plugin
+             [clj :as p]
+             [github :as gh]]))
 
-(p/deps-library)
+[(p/deps-library)
+ (gh/release-job {:dependencies ["test"]})]
